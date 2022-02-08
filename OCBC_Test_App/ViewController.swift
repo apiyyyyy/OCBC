531416a6f2e7f8cfed2362268a515ce0e76707a1
afi permana <afipermana13@live.com>
2022-02-08 23:21:09 +0700

    Initial Commit

---
+++ OCBC_Test_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCBC_Test_App
+//
+//  Created by Family on 08/02/22.
+//  Copyright © 2022 Afi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
